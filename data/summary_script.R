fee75023553e35ec5f0167da179569af9659fa94
Keyvyn Rogers <keyvynrogers@Keyvyns-MacBook-Air.local>
2022-11-14 13:41:46 -0800

    shows

--- data/summary_script.R
+++ data/summary_script.R
@@ -17,3 +17,24 @@ View(tv_shows)
 
 #Creating list about Tv shows that reveals information
 
+# Returning five different metrics from tv shows
+library(dplyr)
+
+number_of_shows_Netflix <- tv_shows %>%  
+  count(Netflix)
+print(number_of_shows_per_service)
+
+number_of_shows_Hulu <- tv_shows %>%  
+  count(Hulu)
+
+number_of_shows_Disney <- tv_shows %>%  
+  count(Disney.)
+
+number_of_shows_Amazon <- tv_shows %>%  
+  count(Prime.Video)
+
+count_combo <- list(number_of_shows_Netflix, number_of_shows_Amazon,
+                         number_of_shows_Disney, number_of_shows_Hulu)
+
+View(count_combo)
+
