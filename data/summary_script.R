cf493fa7db5e2536f2a3fae9a5490be25369a402
Keyvyn Rogers <keyvynrogers@Keyvyns-MacBook-Air.local>
2022-11-14 13:28:49 -0800

    new files

---
+++ data/summary_script.R
@@ -0,0 +1,19 @@
+#Summary Information Script
+#Saveth Thy Wage
+#11/14/2022
+#Authors : John, Dominick, Keyvyn
+
+
+#Dataset 1
+movies_streaming <- read.csv(
+"https://raw.githubusercontent.com/info201a-au2022/project-group-5-section-ag/main/data/MoviesOnStreamingPlatforms.csv") 
+
+#Dataset 2
+tv_shows <- read.csv(
+"https://raw.githubusercontent.com/info201a-au2022/project-group-5-section-ag/main/data/tv_shows.csv")
+
+View(movies_streaming)
+View(tv_shows)
+
+#Creating list about Tv shows that reveals information
+
