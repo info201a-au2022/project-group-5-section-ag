e3b52963270589b4dcb3a97e08375da5fbbb5402
IblameDom19 <hudsdomi@uw.edu>
2022-11-14 22:38:47 -0800

    change data in source

--- data/summary_script.R
+++ data/summary_script.R
@@ -9,11 +9,10 @@ movies_streaming <- read.csv(
 "https://raw.githubusercontent.com/info201a-au2022/project-group-5-section-ag/main/data/MoviesOnStreamingPlatforms.csv") 
 
 #Dataset 2
-tv_shows <- read.csv(
-"https://raw.githubusercontent.com/info201a-au2022/project-group-5-section-ag/main/data/tv_shows.csv")
+tv_shows <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-5-section-ag/main/data/Tv_data_long.csv")
 
 View(movies_streaming)
-View(tv_shows)
+View(tv_data_long)
 
 #Creating list about Tv shows that reveals information
 
@@ -22,25 +21,25 @@ library(dplyr)
 
 #Simple Summary State for shows
 
-summary_info <- list()
-summary_info$num_observations <- nrow(tv_shows)
+Summary<- tv_data_long%>% group_by(Platform) %>% summarise(avg_RT = mean(Rotten_Tomatoes_new), avg_IMDB = mean(IMDb_new, na.rm = TRUE))
+mean(a$Rotten_Tomatoes_new, na.rm= T)
 
 #Number of shows per service
-number_of_shows_Netflix <- tv_shows %>%  
-  filter(Netflix == 1) %>% 
-  count(Netflix)
+number_of_shows_Netflix <- tv_data_long %>%  
+  filter(Platform == "Netflix") %>% 
+  count(Platform)
 
-number_of_shows_Hulu <- tv_shows %>%  
-  filter(Hulu == 1) %>% 
-  count(Hulu)
+number_of_shows_Hulu <- tv_data_long %>%  
+  filter(Platform=="Hulu" ) %>% 
+  count(Platform)
 
-number_of_shows_Disney <- tv_shows %>%  
-  filter(Disney. == 1) %>% 
-  count(Disney.)
+number_of_shows_Disney <- tv_data_long %>%  
+  filter(Platform == "Disney.") %>% 
+  count(Platform)
 
-number_of_shows_Amazon <- tv_shows %>%  
-  filter(Prime.Video == 1) %>% 
-  count(Prime.Video)
+number_of_shows_Amazon <- tv_data_long %>%  
+  filter(Platform == "Prime.Video") %>% 
+  count(Platform)
 
 shows_per_service <- data.frame(name_of_streaming_service = c("Netflix","Hulu","Amazon","Disney"),
                                 number_of_shows = c(1971,1621,1831,351))
