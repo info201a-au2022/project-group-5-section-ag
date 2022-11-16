5c12591fa1c2b797138d48db3ef42d215db40135
IblameDom19 <hudsdomi@uw.edu>
2022-11-16 12:47:12 -0800

    organized aand cleaned more

--- data/summary_script.R
+++ data/summary_script.R
@@ -3,17 +3,20 @@
 #11/14/2022
 #Authors : John, Dominick, Keyvyn
 
-
+#########################################
+#########################################
 #Dataset 1
 movies_streaming <- read.csv(
 "https://raw.githubusercontent.com/info201a-au2022/project-group-5-section-ag/main/data/MoviesOnStreamingPlatforms.csv") 
 
 #Dataset 2
 tv_shows <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-5-section-ag/main/data/Tv_data_long.csv")
-
+ #install.packages("devtools")
+devtools::install_github("R-CoderDotCom/cyberpunk")
 View(movies_streaming)
 View(tv_data_long)
 library(dplyr)
+library(ggplot2)
 #Creating list about Tv show data set that reveals information (summarize)
 
 # Returning five different metrics from tv shows
@@ -56,9 +59,107 @@ tv_data_long%>% group_by(Platform) %>% summarise(avg_Rotten_Tomato = mean(Rotten
                                                  Lowest_Rated_IMDb = min(IMDb_new,na.rm = TRUE),
                                                  Highest_Rated_RT = min(Rotten_Tomatoes_new, na.rm = TRUE),
                                                  Standard_Deviation_IMDb = sd(IMDb_new,na.rm = TRUE))
-mean(a$Rotten_Tomatoes_new, na.rm= T)
+
+#########################################
+#########################################
+#### Pie Chart graph##############
+#### #This Pie Chart attempts to display the number of shows per streaming platform in a way that shows the significance of the difference.
+#We choose a pie chart because it is easy to combine any two or more platforms just like it is easy to buy any two or more platforms.
+#Through this chart you can see that Netflix has by far the most, the next two are close, and Disney has by far the least. With more time/ skill I am going
+#to figure out how to get the actual number to be displayed on each section. 
+
+netflix_count <- all_data %>% 
+  filter(Platform == "Netflix") %>%
+  summarise(count = n())
+
+Hulu_count <- all_data %>% 
+  filter(Platform == "Hulu") %>%
+  summarise(count = n())
+
+Disney._count <- all_data %>% 
+  filter(Platform == "Disney.") %>%
+  summarise(count = n())
+
+Prime.Video_count <- all_data %>% 
+  filter(Platform == "Prime.Video") %>%
+  summarise(count = n())
+
+Count_per_platform <- data.frame(
+  Platform=c("Netflix", "Disney.", "Prime.Video", "Hulu") , 
+  Number_Of_Shows=c(netflix_count$count, Disney._count$count, Prime.Video_count$count, Hulu_count$count)
+)
+
+ggplot(Count_per_platform, aes(x="", y=Number_Of_Shows, fill=Platform))+
+  geom_bar(stat = "identity")+
+  coord_polar("y", start=0)+
+  ggtitle("Number Of Shows Per Platform")
+
+### Bar Graph############
+#Bar Graph comparing the average rotten tomato score across streaming services
+
+netflix_rotten_tomato <- all_data %>% 
+  filter(Platform == "Netflix") %>% 
+  summarise(mean = mean(Rotten_Tomatoes_new))
+
+Disney_rotten_tomato <- all_data %>% 
+  filter(Platform == "Disney.") %>% 
+  summarise(mean = mean(Rotten_Tomatoes_new))
+
+Prime.Video_rotten_tomato <- all_data %>% 
+  filter(Platform == "Prime.Video") %>% 
+  summarise(mean = mean(Rotten_Tomatoes_new))
+
+Hulu_rotten_tomato <- all_data %>% 
+  filter(Platform == "Hulu") %>% 
+  summarise(mean = mean(Rotten_Tomatoes_new))
+
+Average_per_platform <- data.frame(
+  Platform=c("Netflix", "Disney.", "Prime.Video", "Hulu") , 
+  Rotten_Tomato_Score=c(netflix_rotten_tomato$mean, Disney_rotten_tomato$mean, Prime.Video_rotten_tomato$mean, Hulu_rotten_tomato$mean)
+)
+
+ggplot(Average_per_platform, aes(x=Platform, y=Rotten_Tomato_Score))+
+  geom_bar(stat = "identity")+
+  ggtitle("Rotten Tomato Score By Service")
+
+# Data
+Average_per_platform <- data.frame(x = c("Disney","Hulu","Netflix","Prime.Video"),
+                 y = c(1:4))
+
+# Barplot
+cyber_bars(Average_per_platform, area = FALSE, bg.col = "black")
 
 
+####### Scatter Plot######
+#Scatterplot comparing different IMDb ratings across streaming services
+
+netflix_IMDb <- all_data %>% 
+  filter(Platform == "Netflix") %>% 
+  summarise(mean = mean(IMDb_new, na.rm = TRUE))
+
+Disney_IMDb <- all_data %>% 
+  filter(Platform == "Disney.") %>% 
+  summarise(mean = mean(IMDb_new, na.rm = TRUE))
+
+Prime.Video_IMDB <- all_data %>% 
+  filter(Platform == "Prime.Video") %>% 
+  summarise(mean = mean(IMDb_new, na.rm = TRUE))
+
+Hulu_IMDb <- all_data %>% 
+  filter(Platform == "Hulu") %>% 
+  summarise(mean = mean(IMDb_new, na.rm = TRUE))
+
+Average_per_platform2 <- data.frame(
+  Platform=c("Netflix", "Disney.", "Prime.Video", "Hulu") , 
+  IMDb_Score=c(netflix_IMDb$mean, Disney_IMDb$mean, Prime.Video_IMDB$mean, Hulu_IMDb$mean)
+)
+
+
+ggplot(Average_per_platform2, aes(x=Platform, y=IMDb_Score))+
+  geom_point(stat = "identity")+
+  ggtitle("IMDb Score By Service")
+#################################################
+##################################################
 family_friendly <- tv_shows %>% 
   mutate(across('Age',str_replace,'\\+',"")) %>% 
   group_by(Year , Age) %>% 
