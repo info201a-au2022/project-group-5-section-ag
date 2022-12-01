24efc20932737e89bc85e86cad2bea14abe48016
IblameDom19 <hudsdomi@uw.edu>
2022-11-30 16:07:12 -0800

    updates to include Movies

--- source/BarGraph.R
+++ source/BarGraph.R
@@ -1,35 +1,70 @@
 
-#Bar Graph comparing the average rotten tomato score across streaming services
+#Bar Graph comparing the average Tv rating given rotten tomato score across streaming services
 
 bar_all_data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-5-section-ag/main/data/Tv_data_long.csv")
+
+
+#Bar Graph comparing the average Movie rating given rotten tomato score across streaming services
+bar_movie_data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-5-section-ag/main/data/Movie_data_long.csv")
 
 library(ggplot2)
 
 ?summarise
-
-netflix_rotten_tomato <- bar_all_data %>% 
+# Bar graph filtering for Tv shows by platform
+netflix_rotten_tomato_tv <- bar_all_data %>% 
   filter(Platform == "Netflix") %>% 
   summarise(mean = mean(Rotten_Tomatoes_new))
 
-Disney_rotten_tomato <- bar_all_data %>% 
+Disney_rotten_tomato_tv <- bar_all_data %>% 
   filter(Platform == "Disney.") %>% 
   summarise(mean = mean(Rotten_Tomatoes_new))
 
-Prime.Video_rotten_tomato <- bar_all_data %>% 
+Prime.Video_rotten_tomato_tv  <- bar_all_data %>% 
   filter(Platform == "Prime.Video") %>% 
   summarise(mean = mean(Rotten_Tomatoes_new))
 
-Hulu_rotten_tomato <- bar_all_data %>% 
+Hulu_rotten_tomato_tv <- bar_all_data %>% 
   filter(Platform == "Hulu") %>% 
   summarise(mean = mean(Rotten_Tomatoes_new))
 
-Average_per_platform <- data.frame(
+Average_per_platform_tv <- data.frame(
   Platform=c("Netflix", "Disney.", "Prime.Video", "Hulu") , 
-  Rotten_Tomato_Score=c(netflix_rotten_tomato$mean, Disney_rotten_tomato$mean, Prime.Video_rotten_tomato$mean, Hulu_rotten_tomato$mean)
+  Rotten_Tomato_Score=c(netflix_rotten_tomato_tv$mean, Disney_rotten_tomato_tv$mean, Prime.Video_rotten_tomato_tv$mean, Hulu_rotten_tomato_tv$mean)
+)
+###########################
+# Bar graph filtering Movies by platform
+
+netflix_rotten_tomato_movie <- bar_movie_data %>% 
+  filter(Platform == "Netflix") %>% 
+  summarise(mean = mean(Rotten_Tomatoes_new))
+
+Disney_rotten_tomato_movie <- bar_movie_data %>% 
+  filter(Platform == "Disney.") %>% 
+  summarise(mean = mean(Rotten_Tomatoes_new))
+
+Prime.Video_rotten_tomato_movie <- bar_movie_data %>% 
+  filter(Platform == "Prime.Video") %>% 
+  summarise(mean = mean(Rotten_Tomatoes_new))
+
+Hulu_rotten_tomato_movie <- bar_movie_data %>% 
+  filter(Platform == "Hulu") %>% 
+  summarise(mean = mean(Rotten_Tomatoes_new))
+
+Average_per_platform_movie<- data.frame(
+  Platform=c("Netflix", "Disney.", "Prime.Video", "Hulu") , 
+  Rotten_Tomato_Score=c(netflix_rotten_tomato_movie$mean, Disney_rotten_tomato_movie$mean, Prime.Video_rotten_tomato_movie$mean, Hulu_rotten_tomato_movie$mean)
 )
 
-?ggtitle
 
+
+
+
+
+
+
+
+
+#############################
 
 bar_graph_perf <- ggplot(Average_per_platform, aes(x=Platform, y=Rotten_Tomato_Score)) +
   geom_bar(stat = "identity") +
--- source/PieChart.R
+++ source/PieChart.R
@@ -1,29 +1,31 @@
 
-#Pie Chart of number of shows per streaming service
-
+#Pie Chart comparing the average Tv rating given rotten tomato score across streaming services
 
 pie_all_data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-5-section-ag/main/data/Tv_data_long.csv")
 
+#pie Graph comparing the average Movie rating given rotten tomato score across streaming services
+pie_movie_data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-5-section-ag/main/data/Movie_data_long.csv")
 
-?count
 
-netflix_count <- pie_all_data %>% 
+
+
+netflix_count_tv <- pie_all_data %>% 
   filter(Platform == "Netflix") %>%
   summarise(count = n())
 
-Hulu_count <- pie_all_data %>% 
+Hulu_count_tv <- pie_all_data %>% 
   filter(Platform == "Hulu") %>%
   summarise(count = n())
 
-Disney._count <- pie_all_data %>% 
+Disney._count_tv <- pie_all_data %>% 
   filter(Platform == "Disney.") %>%
   summarise(count = n())
 
-Prime.Video_count <- pie_all_data %>% 
+Prime.Video_count_tv <- pie_all_data %>% 
   filter(Platform == "Prime.Video") %>%
   summarise(count = n())
 
-Count_per_platform <- data.frame(
+Count_per_platform_tv <- data.frame(
   Platform=c("Netflix", "Disney.", "Prime.Video", "Hulu") , 
   Number_Of_Shows=c(netflix_count$count, Disney._count$count, Prime.Video_count$count, Hulu_count$count)
 )
@@ -32,8 +34,37 @@ num_of_shows <- ggplot(Count_per_platform, aes(x="", y=Number_Of_Shows, fill=Pla
   geom_bar(stat = "identity")+
   coord_polar("y", start=0)+
   ggtitle("Number Of Shows Per Platform")
-  
-  
+#############################################################
+# movie pie chart code
+
+netflix_count_movie <- pie_movie_data %>% 
+  filter(Platform == "Netflix") %>%
+  summarise(count = n())
+
+Hulu_count_movie <- pie_movie_data %>% 
+  filter(Platform == "Hulu") %>%
+  summarise(count = n())
+
+Disney._count_movie <- pie_movie_data %>% 
+  filter(Platform == "Disney.") %>%
+  summarise(count = n())
+
+Prime.Video_count_movie <- pie_movie_data %>% 
+  filter(Platform == "Prime.Video") %>%
+  summarise(count = n())
+
+Count_per_platform_movie <- data.frame(
+  Platform=c("Netflix", "Disney.", "Prime.Video", "Hulu") , 
+  Number_Of_movies=c(netflix_count_movie$count, Disney._count_movie$count, Prime.Video_count_movie$count, Hulu_count_movie$count)
+)
+
+num_of_movies <- ggplot(Count_per_platform_movie, aes(x="", y=Number_Of_movies, fill=Platform))+
+  geom_bar(stat = "identity")+
+  coord_polar("y", start=0)+
+  ggtitle("Number Of movies Per Platform")
+
+
+  ###########################################################
 #This Pie Chart attempts to display the number of shows per streaming platform in a way that shows the significance of the difference.
 #We choose a pie chart because it is easy to combine any two or more platforms just like it is easy to buy any two or more platforms.
 #Through this chart you can see that Netflix has by far the most, the next two are close, and Disney has by far the least. With more time/ skill I am going
